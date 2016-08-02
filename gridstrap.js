e915f053c51576cc280358950e8fb8eb55f6fa1d
jo <jo@surikat.pro>
2016-08-02 21:07:59 +0200

    start rebuilding on gridEditor

--- gridstrap.js
+++ gridstrap.js
@@ -1,65 +1,151 @@
 (function($){
 
 	var Gridstrap = function(el, opts) {
-		var self = this;
 		this.container = $(el);
 		this.opts = $.extend(opts || {}, {
-			width: 12,
-			cellHeight: 80,
-			itemClass: 'grid-strap-item',
-            placeholderClass: 'grid-strap-placeholder',
-            //handle: '.grid-strap-item-content',
+			
 		});
+		
+		var self = this;
+		var container = this.container;
+		
+		container.addClass('ge-canvas');
+		container.addClass('ge-editing');
 
-		this.placeholder = $(
-			'<div class="' + this.opts.placeholderClass + ' ' + this.opts.itemClass + '">' +
-			'<div class="placeholder-content"></div></div>'
-		);
-		this.placeholder.hide();
+		//$(document)
+			//.on('click', '.gm-preview', function() {
+				//$(this).toggleClass('active btn-danger').trigger('mouseleave');
+			//})
+			//.on('mouseenter', '.gm-preview', function() {
+				//container.removeClass('ge-editing');
+			//})
+			//.on('mouseleave', '.gm-preview', function() {
+				//if (!$(this).hasClass('active')) {
+					//container.addClass('ge-editing');
+				//}
+			//})
+		//;
 		
-		$(document).resize(function(){
-			self.container.sortable("option", "grid", self.getGridIntervals());
-		});
-		
-		self.container.sortable({
-			helper: 'clone',
-			revert: 'invalid',
-			grid: self.getGridIntervals(),
-			placeholder: this.opts.placeholderClass,
-			change: function(event, ui) {
-				ui.placeholder.css({visibility: 'visible', border : '1px solid yellow'});
-			},
-			over: function(e, ui){
-				
-			},
-			out: function(e, ui){
-				
-			},
-			receive: function(e, ui){
-				
-			},
-		});
-		
+        self.init();
+
 	};
 	
-	Gridstrap.prototype.addWidget = function(el,x,y,width,height){
-		el.attr('data-gs-x',x);
-		el.attr('data-gs-y',y);
-		el.attr('data-gs-width',width);
-		el.attr('data-gs-height',height);
-		el.addClass('grid-strap-item');
+	Gridstrap.prototype.createTool = function(drawer, title, className, iconClass, eventHandlers){
+		var tool = $('<a title="' + title + '" class="' + className + '"><span class="' + iconClass + '"></span></a>')
+			.appendTo(drawer)
+		;
+		if (typeof eventHandlers == 'function') {
+			tool.on('click', eventHandlers);
+		}
+		if (typeof eventHandlers == 'object') {
+			$.each(eventHandlers, function(name, func) {
+				tool.on(name, func);
+			});
+		}
+		
+	};
+	Gridstrap.prototype.init = function(){
+		var self = this;
+		var container = this.container;
+		//create row controls
+		container.find('.row').each(function() {
+			var row = $(this);
+			if (row.find('> .ge-tools-drawer').length) { return; }
+
+			var drawer = $('<div class="ge-tools-drawer" />').prependTo(row);
+			self.createTool(drawer, 'Move', 'ge-move', 'fa fa-move');
+			self.createTool(drawer, 'Remove row', '', 'fa fa-close', function() {
+				row.slideUp(function() {
+					row.remove();
+				});
+			});
+			self.createTool(drawer, 'Add column', 'ge-add-column', 'fa fa-plus-circle', function() {
+				row.append('<div data-col="3" />');
+				self.init();
+			});
+		});
+		
+		//create col controls
+		container.find('[data-col]').each(function() {
+			var col = $(this);
+			if (col.find('> .ge-tools-drawer').length) { return; }
+			
+			//col.addClass('col-md-'+col.attr('data-col'));
+			
+			var drawer = $('<div class="ge-tools-drawer" />').prependTo(col);
+
+			self.createTool(drawer, 'Move', 'ge-move', 'fa fa-arrows');
+
+			self.createTool(drawer, 'Make column narrower\n(hold shift for min)', 'ge-decrease-col-width', 'fa fa-minus', function(e) {
+				var size = (parseInt(col.attr('data-col'),10) || 1) - 1;
+				if(size<1) return;
+				col.attr('data-col' ,size);
+			});
+
+			self.createTool(drawer, 'Make column wider\n(hold shift for max)', 'ge-increase-col-width', 'fa fa-plus', function(e) {
+				var size = (parseInt(col.attr('data-col'),10) || 1) + 1;
+				if(size>12) return;
+				col.attr('data-col' ,size);
+			});
+
+			self.createTool(drawer, 'Settings', '', 'fa fa-pencil', function() {
+				details.toggle();
+			});
+			
+			self.createTool(drawer, 'Remove col', '', 'fa fa-close', function() {
+				col.animate({
+					opacity: 'hide',
+					width: 'hide',
+					height: 'hide'
+				}, 400, function() {
+					col.remove();
+				});
+			});
+
+			self.createTool(drawer, 'Add row', 'ge-add-row', 'fa fa-plus-circle', function() {
+				var row = $('<div class="row" />');
+				col.append(row);
+				row.append('<div data-col="6" /><div data-col="6" />');
+				self.init();
+			});
+
+			var details = $('<div class="details" />').appendTo(drawer);
+		});
+		
+		
+		//make sortable
+		container.find('.row').sortable({
+			items: '> [data-col]',
+			connectWith: '.ge-canvas .row',
+			handle: '> .ge-tools-drawer .ge-move',
+			start: sortStart,
+			helper: 'clone',
+		});
+		container.add(container.find('[data-col]')).sortable({
+			items: '> .row',
+			connectsWith: '.ge-canvas, .ge-canvas [data-col]',
+			handle: '> .ge-tools-drawer .ge-move',
+			start: sortStart,
+			helper: 'clone',
+		});
+
+		function sortStart(e, ui) {
+			ui.placeholder.css({ height: ui.item.outerHeight()});
+		}
+	};
+	
+	Gridstrap.prototype.addWidget = function(el,width){
+		el.attr('data-col',width);
 		this.container.append(el);
-		this.container.sortable('refresh');
+		
+		this.init();
+		//this.container.sortable('refresh');
 	};
-	Gridstrap.prototype.removeWidget = function(el){
-		el.remove();
-		this.container.sortable('refresh');
-	};
-	Gridstrap.prototype.getGridIntervals = function(el){
-		var x = this.container.width/this.opts.width;
-		var y = this.opts.cellHeight;
-		return [ x, y ];
-	};
+	//Gridstrap.prototype.removeWidget = function(el){
+		//el.remove();
+		//this.container.sortable('refresh');
+	//};
+	
 	
 	$.fn.gridstrap = function(opts) {
 		return this.each(function() {
