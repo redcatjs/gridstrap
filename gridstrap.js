53d925350c101ff70c4c02302308c47721ef3303
jo <jo@surikat.pro>
2016-09-17 17:29:48 +0200

    disable cursor at

--- gridstrap.js
+++ gridstrap.js
@@ -232,7 +232,7 @@
 				tolerance: 'pointer',
 				placeholder: 'gs-placeholder',
 				appendTo: document.body,
-				cursorAt: { left: 15, top: 10 },
+				//cursorAt: { left: 15, top: 10 },
 				start: function(e, ui){
 					var item = ui.item;
 					if(self.opts.debugEvents) console.log('start',this);
