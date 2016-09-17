cce781a707106f2529a5d0569f48ecb4cd8c7f58
jo <jo@surikat.pro>
2016-09-18 01:08:42 +0200

    disable debug by default

--- gridstrap.js
+++ gridstrap.js
@@ -37,7 +37,7 @@
 			},
 			boxPadding: 15, //$box-padding .gs-col and .gs-placeholder horizontal padding for autoAdjustWidth calculation
 			gsColTransitionWidth: 400, //$gs-col-transition-width .gs-col{ transition width duration }, .gs-margin{ transition width left }
-			debugEvents: true,
+			debugEvents: false,
 		}, opts || {} );
 		this.itemsSelector = '> .gs-col:not(.gs-clone, .gs-moving)';
 		
